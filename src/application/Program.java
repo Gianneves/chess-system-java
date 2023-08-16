838ad84c3942481fa477ea1514b2a9592aa09da2
Gian Neves <gianneves@hotmail.com>
2023-08-16 20:11:17 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-
+        System.out.println("Chess: ");
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
 
